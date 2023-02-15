37ef20e8e77366aacf2ab1d1ac6ec1ed5c2a120b
Leonardo Elias Souza de Oliverira Meireles <leonardoelias802@gmail.com>
2023-02-15 18:20:47 -0300

    testando se passa o pedido 6

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.delete('/products/:id', produtosController.deleteProduto);
 
 // rota de vendas 
 app.post('/sales', middlewaresValidacao.validacaoVendas, vendasController.postVendas);
-
+// teste
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
